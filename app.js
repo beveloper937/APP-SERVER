4149b56f37bb45fbbc8c7e9feb6c8bb0c2930c88
beveloper937 <qngurgns@naver.com>
2023-06-30 15:41:01 +0900

    fix2

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.post('/user/habit', (req, res) => {
         res.status(400).send('사용자가 존재하지 않습니다.');
       } else {
         const USER_ID = users[0].USER_ID;
-        const Date = req.body.Date;         //어플에서 오는 Date를 Date변수에 저장
-        const convertDate = new Date(Date); //어플에서 오는 Date는 String타입이기에 변형
+        const InputDate = req.body.Date;         //어플에서 오는 Date를 Date변수에 저장
+        const convertDate = new Date(InputDate); //어플에서 오는 Date는 String타입이기에 변형
 
         const query = `INSERT INTO User_habit ( Title, Schedule, Color, StartTime, EndTime, Day, Date, Accumulate, Success, Fail, USER_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
         sequelize.query(query, { replacements: [ Title, Schedule, Color, StartTime, EndTime, Day, convertDate, Accumulate, Success, Fail, USER_ID] })
