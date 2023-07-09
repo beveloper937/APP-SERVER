35e10ea785e3637f223fda6c2feec43d875cb032
beveloper937 <qngurgns@naver.com>
2023-07-09 11:53:55 +0900

    fix

--- app.js
+++ app.js
@@ -69,17 +69,20 @@ app.post('/user/habit', (req, res) => {   //유저의 습관 정보 입력
         const USER_ID = users[0].USER_ID;
 
         const query = `INSERT INTO User_habit (Title, Schedule, Color, StartTime, EndTime, Day, Date, Accumulate, Daily, Success, Fail, USER_ID) VALUES (?, ?, ?, ?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'), ?, ?, ?, ?, ?)`;
-        sequelize.query(query, {
-          replacements: [Title, Schedule, Color, StartTime, EndTime, Day, Date, Accumulate, Daily, Success, Fail, USER_ID],
-        })
+        sequelize.query(query, { replacements: [Title, Schedule, Color, StartTime, EndTime, Day, Date, Accumulate, Daily, Success, Fail, USER_ID] })
           .then(() => {
-            res.send('Data added successfully');
+            const selectQuery = `SELECT LAST_INSERT_ID() as HABIT_ID`;
+            return sequelize.query(selectQuery, { plain: true });
+          })
+          .then((result) => {
+            const HABIT_ID = result.HABIT_ID;
+            res.json({ HABIT_ID });
           })
           .catch((err) => {
             console.error('Failed to execute query:', err);
             res.status(502).send('User_habit INSERT Error');
           });
-      }
+        }
     })
     .catch((err) => {
       console.error('Failed to execute query:', err);
