a1708ab50b9b662019aad27af6a34795b1a19efc
beveloper937 <qngurgns@naver.com>
2023-08-22 22:39:53 +0900

    이벤트 리스너 오류

--- models/user_habit.js
+++ models/user_habit.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const User_Tag = require('./user_tag');
+const UserTag = require('./user_tag');
 const mecab = require('mecab-ya');
 
 class UserHabit extends Sequelize.Model {
@@ -87,47 +87,3 @@ class UserHabit extends Sequelize.Model {
 };
 
 module.exports = UserHabit;
-
-////////////////////////////////////////////////////////////////////////
-
-// 명사 추출 함수 정의
-async function extractNouns(text) {
-    const mecab = new mecab(); // MeCab 객체를 생성합니다.
-    const result = await mecab.nouns(text); // 명사 추출을 수행합니다.
-    return result;
-  }
-  
-  UserHabit.addHook('afterCreate', 'afterCreateHook', async (userHabit, options) => {
-    try {
-      console.log('afterCreate event triggered for UserHabit:', userHabit.toJSON());
-      const extractedNouns = await extractNouns(userHabit.Title);
-      await processExtractedNouns(extractedNouns, userHabit.USER_ID, userHabit.HABIT_ID);
-    } catch (error) {
-      console.error('Error during afterCreate event:', error);
-    }
-  });
-  
-  // 추출한 명사를 처리하는 함수 정의
-  async function processExtractedNouns(nouns, userID, habitID) {
-    try {
-      for (const noun of nouns) {
-        await saveNounToUserTag(userID, habitID, noun);
-      }
-    } catch (error) {
-      console.error('Error during processing extracted nouns:', error);
-    }
-  }
-  
-  // 추출한 명사를 User_Tag 테이블에 저장하는 함수 정의
-  async function saveNounToUserTag(userID, habitID, noun) {
-    try {
-      const userTag = await User_Tag.create({
-        USER_ID: userID,
-        HABIT_ID: habitID,
-        Tag: noun,
-      });
-      console.log(`Saved noun "${noun}" to User_Tag:`, userTag.toJSON());
-    } catch (error) {
-      console.error('Error while saving noun to User_Tag:', error);
-    }
-  }
