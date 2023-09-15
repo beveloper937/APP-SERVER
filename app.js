5f1ef918c3deab010c1593044cb53489b5250e20
beveloper937 <qngurgns@naver.com>
2023-09-15 12:30:17 +0900

    변경

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'development' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500).send(err.message);
 });
 
